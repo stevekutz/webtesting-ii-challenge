b754c7b94ec89884a174049df20a6cce8831144c
stevekutz <stkutz@gmail.com>
2019-07-11 23:08:08 -0500

    refactored onClick for fouls in Dashboard

--- app/src/components/Dashboard.js
+++ app/src/components/Dashboard.js
@@ -9,7 +9,7 @@ class Dashboard extends React.Component {
                 <button onClick = {this.props.handleStrikes} > Strike </button>
                 <button onClick = {this.props.handleBalls}>Ball</button>
                 <button onClick = {this.props.handleHits}> Hits </button>
-                <button onClick = {this.props.handleFouls}>Fouls</button>
+                <button onClick = {() => this.props.handleFouls()}>Fouls</button>
            </div> 
         )
     }
